acf28f2d62f24c509c90ed5f0299386a0fcacb96
Moti Zilberman <motiz88@gmail.com>
2016-02-02 17:29:32 +0200

    Test: Fix babel cache path

--- test/utils/babel-register.js
+++ test/utils/babel-register.js
@@ -1,5 +1,5 @@
 if (!process.env.BABEL_CACHE_PATH)
-    process.env.BABEL_CACHE_PATH = require('path').join(__dirname, '.babel-cache');
+    process.env.BABEL_CACHE_PATH = require('path').join(__dirname, '.babel-cache', 'cache');
 require('babel-core/register')({
     only: /jsgrest[\\\/](src|test)/
 });
