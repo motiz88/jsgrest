968f9e99d178acd969e15784f4352a0fa1387371
Moti Zilberman <motiz88@gmail.com>
2016-02-02 16:31:18 +0200

    Test: Setup babel-register caching

--- test/utils/babel-register.js
+++ test/utils/babel-register.js
@@ -1,3 +1,4 @@
+process.env.BABEL_CACHE_PATH = process.env.BABEL_CACHE_PATH || require('path').join(__dirname, '.babel-cache');
 require('babel-core/register')({
     only: /jsgrest[\\\/](src|test)/
 });
