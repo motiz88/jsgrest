efad4d5b0994c442406ba1a9457b17317b720388
Moti Zilberman <motiz88@gmail.com>
2016-02-07 21:01:39 +0200

    (test) Ensure database can be dropped before recreating it

--- test/fixtures/database.sql
+++ test/fixtures/database.sql
@@ -1,5 +1,9 @@
+\timing off
+
 SET client_min_messages TO WARNING;
 
+update pg_database set datallowconn = 'false' where datname = 'postgrest_test';
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'postgrest_test';
 DROP DATABASE IF EXISTS postgrest_test;
 DROP ROLE IF EXISTS postgrest_test;
 CREATE USER postgrest_test createdb createrole;
