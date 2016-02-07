d66c114bed24f9aa40467775a491e758122a96e8
Moti Zilberman <motiz88@gmail.com>
2016-02-07 20:56:37 +0200

    (select) Treat failure in requestToReadStatement as HTTP 400 Bad Request

--- src/handlers/select.js
+++ src/handlers/select.js
@@ -2,6 +2,13 @@ import wrap from '../wrap';
 import requestToReadStatement from '../query/requestToReadStatement';
 
 export default wrap(async function selectHandler(req, res, next) {
-    await res.status(200).sendSelectQuery(...requestToReadStatement(req));
+    let statement;
+    try {
+        statement = requestToReadStatement(req);
+    }
+    catch(e) {
+        return res.sendStatus(400);
+    }
+    await res.status(200).sendSelectQuery(...statement);
     next();
 });
