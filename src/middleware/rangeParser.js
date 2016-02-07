b63c1dfd034afa29f6d5ea9896b8bba95a67dc68
Moti Zilberman <motiz88@gmail.com>
2016-02-07 20:57:11 +0200

    (range) Fail with 416 on first > last

--- src/middleware/rangeParser.js
+++ src/middleware/rangeParser.js
@@ -2,5 +2,11 @@ import parseUnitAndRange from '../parseUnitAndRange';
 
 export default function rangeParser(req, res, next) {
     req.range = parseUnitAndRange(req.header('Range-Unit'), req.header('Range'));
-    next();
+    if (req.range.first > req.range.last)
+        res.sendStatus(416);
+    else {
+        if (req.flags && req.flags.preferSingular)
+            req.range.last = req.range.first;
+        next();
+    }
 }
