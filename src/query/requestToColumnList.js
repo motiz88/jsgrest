f30788a08fd36a135e81f258e716f22e2e3c618c
Moti Zilberman <motiz88@gmail.com>
2016-02-18 17:18:31 +0200

    requestToColumnList

---
+++ src/query/requestToColumnList.js
@@ -0,0 +1,16 @@
+import pgEscape from 'pg-escape';
+import fieldPathToSql from '../fieldPathToSql';
+import selectFieldsParser from '../peg/selectFields';
+
+export default function requestToColumnList(req) {
+    const selectFields =  selectFieldsParser.parse((req.query || {}).select || '')
+        .map(field => {
+            const name = fieldPathToSql(field.name);
+            if (field.cast)
+                return `name::${pgEscape.ident(field)}`;
+            else
+                return name;
+        })
+        .join(', ');
+    return selectFields || '*';
+}
---
+++ test/unit/requestToColumnList.js
@@ -0,0 +1,45 @@
+import requestToColumnList from '../../src/query/requestToColumnList';
+import {expect} from 'chai';
+
+describe('requestToColumnList', function() {
+    it('should be a function', function() {
+        expect(requestToColumnList).to.exist
+            .and.to.be.a('function');
+    });
+
+    it('should return * if select unspecified', function() {
+        requestToColumnList({})
+            .should.equal('*');
+        requestToColumnList({query: {}})
+            .should.equal('*');
+        requestToColumnList({query: {select: ''}})
+            .should.equal('*');
+    });
+
+    it('?select=Col --> "Col"', function() {
+        requestToColumnList({query: {select: 'Col'}})
+            .should.equal('"Col"');
+    });
+
+    it('?select=ColA,ColB --> "ColA", "ColB"', function() {
+        requestToColumnList({query: {select: 'ColA,ColB'}})
+            .should.equal('"ColA", "ColB"');
+    });
+
+    it('?select=ColB,ColA --> "ColB", "ColA"', function() {
+        requestToColumnList({query: {select: 'ColB,ColA'}})
+            .should.equal('"ColB", "ColA"');
+    });
+
+    it('ignore whitespace', function() {
+        requestToColumnList({query: {select: 'a, b, C '}})
+            .should.equal('a, b, "C"');
+    });
+
+    it('escapes anything suspicious', function() {
+        requestToColumnList({query: {select: '* FROM Students; DROP TABLE Students; --'}})
+            .should.equal('"* FROM Students; DROP TABLE Students; --"');
+        requestToColumnList({query: {select: '*" FROM Students; DROP TABLE Students; --'}})
+            .should.equal('"*"" FROM Students; DROP TABLE Students; --"');
+    });
+});
