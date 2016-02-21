450a619988ebe5f327f052ccd42f368646fcccfd
Moti Zilberman <motiz88@gmail.com>
2016-02-21 16:15:05 +0200

    Add test for error response on bad connection string

--- test/integration/basic.js
+++ test/integration/basic.js
@@ -13,14 +13,14 @@ describe('App', function() {
     });
 
 
-    let app;
-
     it('factory (createApp) should be a function', function() {
         expect(createApp).to.exist
             .and.to.be.a('function');
     });
 
     describe('instance', function() {
+        let app;
+
         before(function() {
             app = createApp({connectionString: dbFixtures.connectionString, schema: 'public'});
         });
@@ -34,6 +34,20 @@ describe('App', function() {
             const res = await chai.request(app)
                 .get('/');
             res.should.have.status(200);
+        });
+    });
+
+    describe('instance with bad connection string', function() {
+        let app;
+
+        before(function() {
+            app = createApp({connectionString: 'postgres://nosuchhost', schema: 'public'});
+        });
+
+        it('should respond to GET / with failure', function() {
+            return chai.request(app)
+                .get('/')
+                .should.eventually.be.rejected;
         });
     });
 });
