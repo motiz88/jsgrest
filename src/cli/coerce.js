e12f668b0642143afedd9a9e881738a77bb2c18e
Moti Zilberman <motiz88@gmail.com>
2016-02-21 16:04:17 +0200

    Remove unused function cli/coerce/coerceRegex

--- src/cli/coerce.js
+++ src/cli/coerce.js
@@ -1,14 +1,5 @@
 import url from 'url';
 
-export function coerceRegex(regex) {
-    return value => {
-        const matches = regex.exec(value);
-        if (!matches)
-            throw new Error(`Invalid value '${value}'`);
-        return matches[1] || matches[0];
-    };
-}
-
 export function coerceUrl() {
     return value => {
         const {protocol} = url.parse(value);
@@ -20,9 +11,7 @@ export function coerceUrl() {
 
 export default function coerce(format, displayName) {
     let func;
-    if (format instanceof RegExp)
-        func = coerceRegex(format);
-    else if (typeof format === 'string') {
+    if (typeof format === 'string') {
         displayName = displayName || format;
         switch (format.toLowerCase()) {
             case 'url':
