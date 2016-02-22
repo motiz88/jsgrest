f915a812378c78799e75a5ac9402bb431a63f354
Moti Zilberman <motiz88@gmail.com>
2016-02-22 12:27:41 +0200

    Quieter build-test output

--- build-test.js
+++ build-test.js
@@ -16,14 +16,12 @@ glob(pattern)
         return Promise.all(files.map(function(infile) {
             var relfile = path.relative(indir, infile);
             var outfile = path.resolve(outdir, relfile);
-            console.log(infile);
 
             return fsExtra.mkdirpAsync(path.dirname(outfile))
                 .then(function() {
                     if (/\.js$/.test(infile))
                         return fs.readFile(infile, 'utf8')
                             .then(function(source) {
-                                console.log('distify', relfile);
                                 return j(source).find(j.ImportDeclaration)
                                     .replaceWith(function(p) {
                                         return j.importDeclaration(p.node.specifiers,
@@ -36,24 +34,20 @@ glob(pattern)
                                     });
                             })
                             .then(function(transformedCode) {
-                                console.log('babel', relfile);
                                 return babel.transform(transformedCode, {
                                     filename: infile,
                                     sourceRoot: indir,
                                     babelrc: true,
                                     ast: false,
-                                    // extends: babelrc
                                 }).code;
                             })
                             .then(function(transpiledCode) {
-                                console.log('write', relfile);
                                 return fs.writeFile(outfile, transpiledCode)
                                     .then(function() {
                                         console.log(outfile);
                                     });
                             });
                     else {
-                        console.log('copy', relfile);
                         return fsExtra.copyAsync(infile, outfile)
                             .then(function() {
                                 console.log(outfile);
